26b8cad90f61333602e32a4632374bf5eda17d3f
MrCake4 <winstonjreichelt@gmail.com>
2024-01-15 17:48:11 +0100

    Moved start of the timer into the play() method

--- Game.java
+++ Game.java
@@ -1,19 +1,11 @@
 /**
  * SAVING HARAMBE
- * This class is the main class of the "World of Zuul" application.
- * "World of Zuul" is a very simple, text based adventure game.  Users
- * can walk around some scenery. That's all. It should really be extended
- * to make it more interesting!
- * <p>
  * To play this game, create an instance of this class and call the "play"
  * method.
  * <p>
  * This main class creates and initialises all the others: it creates all
  * rooms, creates the parser and starts the game.  It also evaluates and
  * executes the commands that the parser returns.
- *
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
  */
 
 import java.util.Timer;
@@ -34,18 +26,13 @@ public class Game {
         roomManager = new RoomManager();
         player.setCurrentRoom(roomManager.getLobby());
         parser = new Parser();
-        timer.schedule(new TimerCount(), 0, 10000);
     }
-
-    /**
-     * Create all the rooms and link their exits together.
-     */
-    
     /**
      *  Main play routine.  Loops until end of play.
      */
     public void play() {
         //TODO: need to change the Code
+        timer.schedule(new TimerCount(), 0, 10000);
         System.out.println(new Welcome().processCommand(player));
         ImgGeneration img = new ImgGeneration();
 
